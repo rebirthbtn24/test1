5495e3ae8e7f8019b5e5c46fb2a3240966ba7301
rebirthbtn24 <rebirthbtn@gmail.com>
2019-01-07 21:19:41 +0900

    Initial Commit

---
+++ Lesson4Table2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson4Table2
+//
+//  Created by 増田弘毅 on 2019/01/07.
+//  Copyright © 2019 rebirthbtn24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
